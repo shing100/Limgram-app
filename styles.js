f6de301f9cfde14e05f3a0e5103e52bdd8090804
limgeun <shing100@naver.com>
2019-11-10 00:00:36 +0900

    Styles

---
+++ styles.js
@@ -0,0 +1,9 @@
+export default {
+    blackColor: "#262626",
+    grayColor: "#F9F9F9",
+    darkGreyColor: "#999",
+    lightGreyColor: "#c7c7c7",
+    redColor: "#ED4956",
+    blueColor: "#3897f0",
+    darkBlueColor: "#003569"
+}
