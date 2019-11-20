9ad0efda812c99dd1ad4c453f2321c0b5004b0c0
Lim Geun <shing100@naver.com>
2019-11-20 23:36:37 +0900

    Auth Screens

--- navigation/AuthNavigation.js
+++ navigation/AuthNavigation.js
@@ -1,9 +1,15 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import Signup from "../screens/Auth/Signup";
+import Confirm from "../screens/Auth/Confirm";
+import Login from "../screens/Auth/Login";
 import AuthHome from "../screens/Auth/AuthHome";
 
 const AuthNavigation = createStackNavigator(
   {
-    AuthHome
+    AuthHome,
+    Signup,
+    Login,
+    Confirm
   },
   {
     headerMode: "none"
