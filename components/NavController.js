f1da5afb6aeff9d303d52ac4edd05c6f1d47db4e
Lim Geun <shing100@naver.com>
2019-11-28 01:53:09 +0900

    MainNAvigation

--- components/NavController.js
+++ components/NavController.js
@@ -2,13 +2,14 @@ import React from "react";
 import { View } from "react-native";
 import { useIsLoggedIn } from "../AuthContext";
 import AuthNavigation from "../navigation/AuthNavigation";
+import MainNavigation from "../navigation/MainNavigation";
 
 export default () => {
     const isLoggedIn = true;
 
     return (
         <View style={{ flex: "1" }}>
-            {isLoggedIn ? <TabNavifation /> : <AuthNavigation />}
+            {isLoggedIn ? <MainNavigation /> : <AuthNavigation />}
         </View>
     );
 };
