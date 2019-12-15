0626251240881a8573653c434cd9786bf3f77458
Lim Geun <shing100@naver.com>
2019-12-15 21:10:39 +0900

    constansts

--- screens/Auth/AuthHome.js
+++ screens/Auth/AuthHome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import constants from "../../components";
 
 const View = styled.View`
   justify-content: center;
@@ -8,7 +9,30 @@ const View = styled.View`
   flex: 1;
 `;
 
-const Text = styled.Text``;
+const Image = styled.Image`
+  width: ${constants.width / 2.5};
+`;
+
+const Touchable = styled.TouchableOpacity``;
+const SignUpBtn = styled.View`
+  background-color: ${props => props.theme.blueColor};
+  padding: 10px;
+  margin: 0px 50px;
+  border-radius: 4px;
+  width: ${constants.width / 2};
+  margin-bottom: 25px;
+`;
+const SignUpBtnText = styled.Text`
+  color: white;
+  text-align: center;
+  font-weight: 600;
+`;
+
+const LoginLink = styled.View``;
+const LoginLinkText = styled.Text`
+  color: ${props => props.theme.blueColor};
+  font-weight: 600;
+`;
 
 export default ({ navigation }) => (
   <View>
